4817794f2a6238d74c836fc0f7644466352d1d91
Jrevata <jordan.revata@tecsup.edu.pe>
2018-02-26 21:18:28 -0500

    Primer y ultimo cambio

---
+++ app/src/main/java/com/jrevatas2/myfirstapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jrevatas2.myfirstapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this, SplashActivity.class));
+            }
+        }, 3000);
+    }
+}
